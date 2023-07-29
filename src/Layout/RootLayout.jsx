90b9a8a57da54f0b9b6d41eda01882e8f9c682e1
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-07-29 19:32:10 +0530

    fix: sidebar z-index

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -12,7 +12,7 @@ function RootLayout() {
 
   return (
     <div className="relative max-w-7xl mx-auto h-full grid sm:grid-cols-[auto_minmax(200px,_1fr)]">
-      <div className="fixed bottom-0 left-0 right-0 border-solid border-t border-darkGray sm:sticky sm:top-0 sm:bottom-0 sm:h-screen sm:overflow-y-scroll sm:border-r sm:border-t-0 no-scrollbar">
+      <div className="z-10 fixed bottom-0 left-0 right-0 border-solid border-t border-darkGray sm:sticky sm:top-0 sm:bottom-0 sm:h-screen sm:overflow-y-scroll sm:border-r sm:border-t-0 no-scrollbar">
         <SideBar />
       </div>
 
